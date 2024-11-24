43f960f1d9b705bbb646355fb3eff4c89ebfe18f
Lindsey de Vargas de Azevedo <lindseyanjo@gmail.com>
2024-11-24 19:47:12 -0300

    Update script.py

--- script.py
+++ script.py
@@ -122,7 +122,7 @@ if nome_empresa:
 
     # Seleção de categoria
     categoria = st.selectbox("Escolha a categoria da sua empresa:", df['categoria'].unique())
-    st.write(f"{cat[cat['nome'] == categoria]['desc'].vaues}!")
+    st.write(f"{cat[cat['nome'] == categoria]['desc'].values}!")
     
     
     # Seleção de gênero
